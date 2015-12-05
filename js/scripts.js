f28260958590762d8862275590fb9524568dd359
Ellen <lnduncklee@gmail.com>
2015-12-04 20:58:30 -0600

    reverse animate opacity

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,16 @@
 //ADD YOUR CUSTOM JAVASCRIPT AND jQUERY HERE
 $('.gallery').on('click','img', function() {
-   $(this).animate({
-       'width':'50vh'
-   },500);
-   $('img').not($(this)).animate({'opacity':'0.2'}, 500);
+  if($(this).hasClass('expanded')){
+
+    $(this).animate({
+        'width':'15%'
+    },500).removeClass('expanded');
+    $('img').not($(this)).animate({'opacity':'1'}, 500);
+
+  } else {
+     $(this).animate({
+         'width':'50vh'
+     },500).addClass('expanded');
+     $('img').not($(this)).animate({'opacity':'0.2'}, 500);
+  }
 });
